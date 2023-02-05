f74463ef7e8987d4e750bf3d3d010a5285ff64d7
mulitate4 <aryan.dongre1605@gmail.com>
2023-02-06 01:08:16 +0530

    Overhauled UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,130 +1,178 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 import 'package:flutter_alarm_clock/flutter_alarm_clock.dart';
-import 'package:intl/intl.dart';
+import 'package:sleek_circular_slider/sleek_circular_slider.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 void main() {
-  runApp(const AlarmWithYoutube());
+  runApp(const NapTime());
 }
 
-class AlarmWithYoutube extends StatefulWidget {
-  const AlarmWithYoutube({Key? key}) : super(key: key);
+class NapTime extends StatefulWidget {
+  const NapTime({Key? key}) : super(key: key);
 
   @override
-  State<AlarmWithYoutube> createState() => _AlarmWithYoutubeState();
+  State<NapTime> createState() => _NapTimeState();
 }
 
-class _AlarmWithYoutubeState extends State<AlarmWithYoutube> {
-  int _selectedHour = 0;
-  int _selectedMinutes = 0;
+class _NapTimeState extends State<NapTime> {
   int _selectedNapMinutes = 0;
+
+  var textStyle = TextStyle(
+      fontSize: 60,
+      color: Colors.white,
+      fontWeight: FontWeight.bold,
+      shadows: [
+        Shadow(color: Colors.white, offset: Offset(1, 1), blurRadius: 30),
+      ]);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                    decoration:
-                        BoxDecoration(borderRadius: BorderRadius.circular(12)),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: [
-                          const SizedBox(height: 20),
-                          Text("Hour: $_selectedHour"),
-                          Slider(
-                              label: _selectedHour.toString(),
-                              value: _selectedHour.toDouble(),
-                              min: 0.0,
-                              max: 23.0,
-                              divisions: 23,
-                              onChanged: (value) {
-                                setState(() {
-                                  _selectedHour = value.round();
-                                });
-                              }),
-                          const SizedBox(height: 20),
-                          Text("Minutes: $_selectedMinutes"),
-                          Slider(
-                              label: _selectedMinutes.toString(),
-                              value: _selectedMinutes.toDouble(),
-                              min: 0.0,
-                              max: 60.0,
-                              divisions: 60,
-                              onChanged: (value) {
-                                setState(() {
-                                  _selectedMinutes = value.round();
-                                });
-                              }),
-                          TextButton(
-                              onPressed: () {
-                                FlutterAlarmClock.createAlarm(
-                                    _selectedHour, _selectedMinutes);
-                              },
-                              child: const Text("Set Alarm")),
-                        ],
-                      ),
-                    ))),
-            const SizedBox(height: 32),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                  decoration: BoxDecoration(
-                      // border: Border.all(width: 1, color: Colors.grey.shade600),
-                      borderRadius: BorderRadius.circular(12)),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
+        body: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                  colors: [const Color(0xFFcad0ff), const Color(0xFFe3e3e3)])),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Neumorphic(
+                padding: EdgeInsets.all(20),
+                style: NeumorphicStyle(
+                  depth: 10,
+                  color: Colors.transparent,
+                  // boxShape: NeumorphicBoxShape.roundRect(
+                  //     BorderRadius.circular(40)),
+                  boxShape: NeumorphicBoxShape.circle(),
+                ),
+                child: SleekCircularSlider(
+                  initialValue: 5,
+                  max: 120,
+                  appearance: CircularSliderAppearance(
+                      startAngle: 120,
+                      angleRange: 310,
+                      size: 220,
+                      customColors: CustomSliderColors(
+                          dynamicGradient: true,
+                          shadowColor: Colors.grey,
+                          shadowMaxOpacity: .05,
+                          shadowStep: 3,
+                          dotColor: Colors.grey[400],
+                          trackColor: const Color(0xFFfbe9d7),
+                          progressBarColors: [
+                            const Color(0xFFf6d5f7),
+                            const Color(0xFFfbe9d7),
+                          ])),
+                  onChange: (double value) {
+                    setState(() {
+                      _selectedNapMinutes = value.round();
+                    });
+                  },
+                  innerWidget: (double value) {
+                    return Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        const SizedBox(height: 20),
-                        Text("Minutes: $_selectedNapMinutes"),
-                        Slider(
-                            label: _selectedNapMinutes.toString(),
-                            value: _selectedNapMinutes.toDouble(),
-                            min: 0.0,
-                            max: 60.0,
-                            divisions: 60,
-                            onChanged: (value) {
-                              setState(() {
-                                _selectedNapMinutes = value.round();
-                              });
-                            }),
-                        TextButton(
-                            onPressed: () {
-                              DateTime currentTime = DateTime.now();
-                              int _currentMinutes = currentTime.minute;
-                              int _currentHour = currentTime.hour;
-
-                              int _computedMinutes = 0;
-                              int _computedHour = 0;
-
-                              if (_currentMinutes + _selectedNapMinutes > 59) {
-                                _computedMinutes =
-                                    (_currentMinutes + _selectedNapMinutes) -
-                                        60;
-                                _computedHour = _currentHour + 1;
-                              } else {
-                                _computedMinutes =
-                                    _currentMinutes + _selectedNapMinutes;
-                                _computedHour = _currentHour;
-                              }
-
-                              FlutterAlarmClock.createAlarm(
-                                  _computedHour, _computedMinutes);
-                            },
-                            child: const Text("Nap Time")),
+                        Text(
+                          value.round().toString(),
+                          style: textStyle,
+                        ),
+                        Text(
+                          "Minutes",
+                          style: textStyle.copyWith(
+                              fontSize: 20, fontWeight: FontWeight.normal),
+                        ),
                       ],
-                    ),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(height: 40),
+              NeumorphicButton(
+                  style: NeumorphicStyle(depth: 10, color: Colors.transparent),
+                  onPressed: () {
+                    presetSetAlarm(_selectedNapMinutes);
+                  },
+                  child: Text(
+                    "Nap Time",
+                    style: textStyle.copyWith(fontSize: 20, shadows: []),
                   )),
-            ),
-          ],
+              SizedBox(height: 40),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  NeumorphicButton(
+                      padding: EdgeInsets.all(20),
+                      style: NeumorphicStyle(
+                        depth: 10,
+                        color: Colors.transparent,
+                        boxShape: NeumorphicBoxShape.circle(),
+                      ),
+                      onPressed: () {
+                        presetSetAlarm(15);
+                      },
+                      child: Text(
+                        "15",
+                        style: textStyle.copyWith(fontSize: 24, shadows: []),
+                      )),
+                  NeumorphicButton(
+                      padding: EdgeInsets.all(20),
+                      style: NeumorphicStyle(
+                        depth: 10,
+                        color: Colors.transparent,
+                        boxShape: NeumorphicBoxShape.circle(),
+                      ),
+                      onPressed: () {
+                        presetSetAlarm(30);
+                      },
+                      child: Text(
+                        "30",
+                        style: textStyle.copyWith(fontSize: 24, shadows: []),
+                      )),
+                  NeumorphicButton(
+                      padding: EdgeInsets.all(20),
+                      style: NeumorphicStyle(
+                        depth: 10,
+                        color: Colors.transparent,
+                        boxShape: NeumorphicBoxShape.circle(),
+                      ),
+                      onPressed: () {
+                        presetSetAlarm(45);
+                      },
+                      child: Text(
+                        "45",
+                        style: textStyle.copyWith(fontSize: 24, shadows: []),
+                      )),
+                  NeumorphicButton(
+                      padding: EdgeInsets.all(20),
+                      style: NeumorphicStyle(
+                        depth: 10,
+                        color: Colors.transparent,
+                        boxShape: NeumorphicBoxShape.circle(),
+                      ),
+                      onPressed: () {
+                        presetSetAlarm(90);
+                      },
+                      child: Text(
+                        "90",
+                        style: textStyle.copyWith(fontSize: 24, shadows: []),
+                      )),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  void presetSetAlarm(int minutes) {
+    DateTime currentTime = DateTime.now();
+    DateTime alarmTime = currentTime.add(Duration(minutes: minutes));
+
+    FlutterAlarmClock.createAlarm(alarmTime.hour, alarmTime.minute);
   }
 }
